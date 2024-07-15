b12100ad9162a1f730cdd70884c5e86447129e64
CodeCraftRisan <152704503+CodeCraftRisan@users.noreply.github.com>
2024-07-15 08:55:55 +0600

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,12 @@ app.use(
   })
 );
 
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
